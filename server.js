20f6c9e34e05e2e9132d4f872f39ef9f56edee98
Maryussuperboy <martsyalis@gmail.com>
2017-09-18 17:41:26 -0700

    local stuff

--- server.js
+++ server.js
@@ -4,3 +4,5 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(express.static('./public'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
