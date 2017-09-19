b51662637d16a359796b5d662aa3615d9fe05547
Robin Miller <rmillerpdx@gmail.com>
2017-09-19 13:53:29 -0700

    fix repopulation bug in requestWords function

--- public/scripts/models/handleGen.js
+++ public/scripts/models/handleGen.js
@@ -9,18 +9,19 @@ let app = {};
   words.all = [];
 
 
-// this array will contain 3 arrays of data from API
-words.genArray = []
+  // this array will contain 3 arrays of data from API
+  words.genArray = []
 
-let answersArray = ['sl=bird','rel_trg=tiny','rel_trg=pig']
+  let answersArray = ['sl=bird','rel_trg=tiny','rel_trg=pig']
 
   words.requestWords = function (callback){
     answersArray.forEach(function(value){
       $.get('/datamuse/api/' + value)
         .then ( function(data) {
-        console.log("data:",data)
-        // TODO: refactor to clear genArray prior to populating
-        words.genArray.push(data)
+        //Clear existing genArray before repopulating
+        words.genArray = [];
+        console.log("data:",data);
+        words.genArray.push(data);
         console.log("genArray:",words.genArray);
         })
     })
@@ -40,13 +41,13 @@ let answersArray = ['sl=bird','rel_trg=tiny','rel_trg=pig']
     return wordArray;
   }
 
-// append our wordArray to the DOM
-words.appendWords = function ( wordArray ) {
-  wordArray.forEach( function ( arrayEle, currentIndex, array) {
-    let idName = "slot" + (currentIndex+1);
-    $( '#' + idName).text(arrayEle);
-  });
-}
+  // append our wordArray to the DOM
+  words.appendWords = function ( wordArray ) {
+    wordArray.forEach( function ( arrayEle, currentIndex, array) {
+      let idName = "slot" + (currentIndex+1);
+      $( '#' + idName).text(arrayEle);
+    });
+  }
 
   module.words = words
 
