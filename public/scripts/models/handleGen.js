0b2eed8c259357b99118cb1accf437617a072a12
Maryussuperboy <martsyalis@gmail.com>
2017-09-18 17:24:16 -0700

    untested ajax and api

--- public/scripts/models/handleGen.js
+++ public/scripts/models/handleGen.js
@@ -4,8 +4,34 @@ let app = app || {};
 
 (function (module){
 
+  const words = {};
+  
+  words.all = [];
+
+  app.answer = {answer: actualAnswer, category: actualCategory}
+
+  actualAnswer = formReply
 
 
+// array of contatenated category and answer pairs from the user submited form
 
+
+// this array will contain 3 arrays of data from API
+words.genArray = []
+
+  words.requestWords = function (callback){
+    app.answersArray.forEach(function(value){
+      $.get('/datamuse/api/' + value)
+        .then ( function(data) {
+        console.log(data)
+        words.genArray.push(data)
+        console.log(genArray);
+        })
+    })
+      .then (callback);
+  }
+
+
+  module.words = words
 
 })(app);
--- server.js
+++ server.js
@@ -4,3 +4,15 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const requestProxy = require('express-request-proxy');
+
+//calls requestProxy used in app.get
+ function getDatamuse (request, response) {
+  console.log ("getDatamuse is running")
+  (requestProxy ({
+    url:`https://api.datamuse.com/words?${request.params[0]}`
+  })) (request,response)
+}
+
+app.get("datamuse/api/*", getDatamuse )
+
