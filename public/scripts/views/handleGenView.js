ebac57644dbe1d145d8823ebd9a14010014091bd
Robin Miller <rmillerpdx@gmail.com>
2017-09-20 16:52:30 -0700

    create basic switch for concat options. variable concatType temporarily defined in handleGenView.js

--- public/scripts/views/handleGenView.js
+++ public/scripts/views/handleGenView.js
@@ -23,21 +23,29 @@ var app = app || {};
   genView.appendWords = function () {
     app.words.slotArray.forEach( function ( arrayEle, currentIndex, array) {
       let idName = '#slot' + (currentIndex+1);
-
-      //If data-saved
-      if ( $(idName).attr('data-saved') !== 'true' ) { $(idName).text(arrayEle); }
-
-      //Update currentHandle with current displayed handle
-      app.words.currentHandle = '';
-      for (let i = 0; i < $('.slots').children().length; i++){
-        if (app.words.currentHandle === ''){
-          app.words.currentHandle += $('.slots').children().eq(i).text();
-        } else {
-          app.words.currentHandle += '-' + $('.slots').children().eq(i).text();
+      let concatType = 'camelcase';
+      //If data-saved isn't true, append to DOM
+      if ( $(idName).attr('data-saved') !== 'true' ) { 
+        switch (concatType){
+          case 'camelcase':
+            let newElement = arrayEle.split('');
+            newElement[0] = newElement[0].toUpperCase();
+            $(idName).text(newElement.join(''));
         }
         
-        //TODO: refactor to accomodate concationation options
+        // $(idName).text(arrayEle); 
       }
+
+      //Update currentHandle with current displayed handle
+      // app.words.currentHandle = '';
+      // for (let i = 0; i < $('.slots').children().length; i++){
+      //   if (app.words.currentHandle === ''){
+      //     app.words.currentHandle += $('.slots').children().eq(i).text();
+      //   } else {
+      //     app.words.currentHandle += '-' + $('.slots').children().eq(i).text();
+      //   }
+
+      // }
 
       //Add event listener to toggle "data-saved" status
       $(idName).off('click');
