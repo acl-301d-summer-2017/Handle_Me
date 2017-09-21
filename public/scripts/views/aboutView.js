778baf89891ae97331535a99d43f9131afaaa6cf
Morgan LaCouture <morganlea18@gmail.com>
2017-09-20 17:24:26 -0700

    about route working

--- public/scripts/views/aboutView.js
+++ public/scripts/views/aboutView.js
@@ -5,7 +5,10 @@ var app = app || {};
 (function (module) {
 
     const aboutView = {};
-
+    $('footer #about-route').click(function (){
+        console.log('worked');
+        page('/about');
+    });
     // hides survey and generator, shows the about us route
     aboutView.init = function () {
         $('.about').show().siblings().hide()
